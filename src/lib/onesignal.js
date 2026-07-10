69f2534bfe74b3a0cebdd142d62e2f4fa1cf7a8c
kelz2222 <kelechicchukwu34@gmail.com>
2026-07-10 23:16:14 +0000

    Update onesignal.js

--- src/lib/onesignal.js
+++ src/lib/onesignal.js
@@ -1,14 +1,3 @@
-export function initPushNotifications() {
-  if (typeof window === 'undefined') return;
-  window.OneSignalDeferred = window.OneSignalDeferred || [];
-  window.OneSignalDeferred.push(async (OneSignal) => {
-    await OneSignal.init({
-      appId: '4b76d76d-38cf-46bc-8742-6084b7098053',
-      allowLocalhostAsSecureOrigin: true,
-    });
-  });
-}
-
 export function requestPushPermission() {
   if (typeof window === 'undefined') return;
   window.OneSignalDeferred = window.OneSignalDeferred || [];
